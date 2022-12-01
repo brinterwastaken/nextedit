5bc256edca3b8e6f6909e87f8f991dab211a403b
xXTgamerXx <trinethr_n@pm.me>
2022-12-01 23:20:46 +0530

    save shortcut fixed 90%

--- src/main.js
+++ src/main.js
@@ -9,13 +9,14 @@ document.getElementById('openbtn').addEventListener('click', () => openfile())
 document.getElementById('savebtn').addEventListener('click', () => savefile())
 document.getElementById('savebtn').addEventListener('contextmenu', (ev) => {ev.preventDefault(); saveas()})
 
+var platform = await os.platform();
 
-if (await os.platform() === "win32" || "linux") {
+if (platform == "win32" || platform == "linux") {
   document.getElementById('win-buttons').style.display = "flex"
   document.getElementById('win-minimize').addEventListener('click', () => appWindow.minimize())
   document.getElementById('win-maximize').addEventListener('click', () => appWindow.toggleMaximize())
   document.getElementById('win-close').addEventListener('click', () => appWindow.close())
-} else if (await os.platform() == "darwin") {
+} else if (platform == "darwin") {
   document.getElementById("title").style.opacity = 0
   document.getElementById('mac-buttons').style.display = "flex"
   document.getElementById('mac-minimize').addEventListener('click', () => appWindow.minimize())
@@ -54,15 +55,22 @@ const filetypes = [
   }
 ]
 
-document.addEventListener("keydown", (e) => keypressed(e));
-
-function keypressed (e){
-  const {key, metaKey, ctrlKey} = e; 
-  if(key === "s" && (ctrlKey || metaKey)){
-    console.log("save pressed")
-    console.log(e)
+document.addEventListener("keyup", (e) => {
+  const {key, metaKey, ctrlKey, shiftKey} = e; 
+  var modKey;
+  if (platform == "win32" || platform == "linux") {
+    modKey = ctrlKey
+  } else if (platform == "darwin") {
+    modKey = metaKey
   }
-}
+  if(key === "s"  && !shiftKey && modKey){
+    savefile()
+  }
+  if(key === "s" && shiftKey && modKey){
+    saveas()
+  }
+
+});
 
 var filepath
 
